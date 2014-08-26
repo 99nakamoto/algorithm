032089ba987cb43765cb3425d3eb96c48ac1fa28
Michael <theimmortalfish@gmail.com>
2014-08-26 12:27:52 +0800

    d

--- CodeApe/src/common/TreeNode.java
+++ CodeApe/src/common/TreeNode.java
@@ -16,10 +16,10 @@ public class TreeNode {
 
 	public static TreeNode constructBstFromPreorder(int[] preorder) {
 		p = 0;
-		return helper(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		return buildBstHelper(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
 	}
 
-	private static TreeNode helper(int[] A, int min, int max) {
+	private static TreeNode buildBstHelper(int[] A, int min, int max) {
 		int len = A.length;
 		if (p >= len) {
 			return null;
@@ -28,9 +28,40 @@ public class TreeNode {
 		}
 		TreeNode root = new TreeNode(A[p]);
 		p++;
-		root.left = helper(A, min, root.val);
-		root.right = helper(A, root.val, max);
+		root.left = buildBstHelper(A, min, root.val);
+		root.right = buildBstHelper(A, root.val, max);
 		return root;
+	}
+
+	public static TreeNode constructTreeFromPreAndInorder(int[] preorder,
+			int[] inorder) {
+		if (preorder == null || inorder == null
+				|| preorder.length != inorder.length) {
+			return null;
+		}
+		return buildTreeHelper(preorder, 0, preorder.length - 1, inorder, 0,
+				inorder.length - 1);
+	}
+
+	private static TreeNode buildTreeHelper(int[] preorder, int a, int b,
+			int[] inorder, int c, int d) {
+		if (a > b) {
+			return null;
+		}
+		int headVal = preorder[a];
+		TreeNode head = new TreeNode(headVal);
+		int p = c;
+		while (p <= d) {
+			if (inorder[p] == headVal) {
+				break;
+			}
+			p++;
+		}
+		head.left = buildTreeHelper(preorder, a + 1, a + p - c, inorder, c,
+				p - 1);
+		head.right = buildTreeHelper(preorder, b - d + p + 1, b, inorder,
+				p + 1, d);
+		return head;
 	}
 
 	public void printLevelOrder() {
