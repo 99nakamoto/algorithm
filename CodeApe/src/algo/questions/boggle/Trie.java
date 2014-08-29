d4c2c80c058f3476e0dc23b937304f68fb70bd80
Ran <ran@worksap.co.jp>
2014-08-29 20:13:48 +0800

    d

---
+++ CodeApe/src/algo/questions/boggle/Trie.java
@@ -0,0 +1,29 @@
+package algo.questions.boggle;
+
+public class Trie {
+	private TrieNode root;
+
+	public Trie() {
+		this.root = new TrieNode();
+	}
+
+	public void addWord(String word) {
+		TrieNode node = this.root;
+		for (char c : word.toCharArray()) {
+			node = node.addChild(c);
+			if (node == null)
+				return;
+		}
+		node.setWord(true);
+	}
+
+	public TrieNode match(String s) {
+		TrieNode node = this.root;
+		for (char c : s.toCharArray()) {
+			node = node.get(c);
+			if (node == null)
+				return null;
+		}
+		return node;
+	}
+}
