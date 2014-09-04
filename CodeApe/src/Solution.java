12eb7a83468f55f22688dddd6cc30a3544ff2686
Home Mac <codeaperan@gmail.com>
2014-09-04 22:22:52 +0800

    d

---
+++ CodeApe/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+	String s = null;
+
+	public static void main(String[] args) {
+		Solution ins = new Solution();
+		ins.solution();
+	}
+
+	public void solution() {
+		if (s != null & s.length() == 0) {
+			System.out.println("ha ya");
+		}
+	}
+}
