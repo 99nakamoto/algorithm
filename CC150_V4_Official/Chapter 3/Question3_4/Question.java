6debe31190015937f0e30f1ac05442fe742338df
Home Mac <codeaperan@gmail.com>
2014-09-06 16:36:55 +0800

    d

--- CC150_V4_Official/Chapter 3/Question3_4/Question.java
+++ CC150_V4_Official/Chapter 3/Question3_4/Question.java
@@ -1,12 +1,64 @@
 package Question3_4;
 
+import java.util.Stack;
+
 public class Question {
+
 	public static void main(String[] args) {
 		// Set up code.
-		int n = 5;
-		Tower[] towers = new Tower[n];
-		for (int i = 0; i < 3; i++) towers[i] = new Tower(i);
-		for (int i = n - 1; i >= 0; i--) towers[0].add(i);
-		towers[0].moveDisks(n, towers[2], towers[1]);
+		int numDisks = 3;
+		Tower[] tower = new Tower[3];
+		for (int id = 0; id < 3; id++) {
+			tower[id] = new Tower(id);
+		}
+		for (int diskSize = numDisks - 1; diskSize >= 0; diskSize--) {
+			tower[0].add(diskSize);
+		}
+		tower[0].moveDisks(numDisks, tower[2], tower[1]);
+	}
+}
+
+class Tower {
+
+	private Stack<Integer> disks;
+	private int index;
+
+	public Tower(int id) {
+		disks = new Stack<Integer>();
+		index = id;
+	}
+
+	public int index() {
+		return index;
+	}
+
+	public void add(int d) {
+		if (!disks.isEmpty() && disks.peek() <= d) {
+			System.out.println("Error placing disk " + d);
+		} else {
+			disks.push(d);
+		}
+	}
+
+	public void moveTopTo(Tower t) {
+		int top = disks.pop();
+		t.add(top);
+		System.out.println("Disk[" + top + "]: Rod" + index() + "-->"
+				+ t.index());
+	}
+
+	public void print() {
+		System.out.println("Contents of Tower " + index());
+		for (int i = disks.size() - 1; i >= 0; i--) {
+			System.out.println("   " + disks.get(i));
+		}
+	}
+
+	public void moveDisks(int n, Tower destination, Tower buffer) {
+		if (n > 0) {
+			moveDisks(n - 1, buffer, destination);
+			moveTopTo(destination);
+			buffer.moveDisks(n - 1, destination, this);
+		}
 	}
 }
