2f3402de33352332f098cb4107e6865b9c7c01b9
CodeApe <codeaperan@gmail.com>
2014-09-07 21:46:28 +0800

    Signed-off-by: CodeApe <codeaperan@gmail.com>

--- CC150_V4_Official/Chapter 9/Question9_5/Question.java
+++ CC150_V4_Official/Chapter 9/Question9_5/Question.java
@@ -1,9 +1,23 @@
 package Question9_5;
 
-import java.util.ArrayList;
-
 public class Question {
-	public static int search(String[] strings, String str, int first, int last) {
+
+	public static int search(String[] input, String target) {
+		if (input == null || target == null) {
+			return -1;
+		}
+		if (target == "") {
+			for (int i = 0; i < input.length; i++) {
+				if (input[i] == "") {
+					return i;
+				}
+			}
+			return -1;
+		}
+		return search(input, target, 0, input.length - 1);
+	}
+
+	private static int search(String[] strings, String str, int first, int last) {
 		while (first <= last) {
 			// Ensure there is something at the end
 			while (first <= last && strings[last] == "") {
@@ -17,7 +31,8 @@ public class Question {
 				++mid; // will always find one
 			}
 			int r = strings[mid].compareTo(str);
-			if (r == 0) return mid;
+			if (r == 0)
+				return mid;
 			if (r < 0) {
 				first = mid + 1;
 			} else {
@@ -26,26 +41,24 @@ public class Question {
 		}
 		return -1;
 	}
-		
-	public static int search(String[] strings, String str) {
-		if (strings == null || str == null) {
-			return -1;
-		}
-		if (str == "") {
-			for (int i = 0; i < strings.length; i++) {
-				if (strings[i] == "") {
-					return i;
-				}
-			}
-			return -1;
-			}
-		return search(strings, str, 0, strings.length - 1);
-	}
-	
+
 	public static void main(String[] args) {
-        String[] stringList = {"apple", "", "", "banana", "", "", "", "carrot", "duck", "", "", "eel", "", "flower"};
+		String[] stringList = { "apple", "", "", "banana", "", "", "",
+				"carrot", "duck", "", "", "eel", "", "flower" };
+
 		for (String s : stringList) {
-        	System.out.println("<" + s + "> " + " appears at location " + search(stringList, s));
+			System.out.println("<" + s + "> " + " appears at location "
+					+ search(stringList, s));
 		}
+
+		String s = "harry";
+		printMsg(s, search(stringList, s));
+
+		s = "-343";
+		printMsg(s, search(stringList, s));
+	}
+
+	private static void printMsg(String s, int result) {
+		System.out.println("<" + s + "> " + " appears at location " + result);
 	}
 }
