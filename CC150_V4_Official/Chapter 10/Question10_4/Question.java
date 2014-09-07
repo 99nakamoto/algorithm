ec04d7254d54054c9b7fac3e39b4a658bee326ad
Home Mac <codeaperan@gmail.com>
2014-09-08 01:53:44 +0800

    d

--- CC150_V4_Official/Chapter 10/Question10_4/Question.java
+++ CC150_V4_Official/Chapter 10/Question10_4/Question.java
@@ -1,6 +1,7 @@
 package Question10_4;
 
 public class Question {
+
 	/* Flip a positive sign to negative, or a negative sign to pos */
 	public static int FnNegate(int a) {
 		int neg = 0;
@@ -9,7 +10,7 @@ public class Question {
 			neg += d;
 			a += d;
 		}
-	    return neg;
+		return neg;
 	}
 
 	/* Subtract two numbers by negating b and adding them */
@@ -19,34 +20,43 @@ public class Question {
 
 	/* Check if a and b are different signs */
 	public static boolean DifferentSigns(int a, int b) {
-		return ((a < 0 && b > 0) || (a > 0 && b < 0)) ? true : false; 
+		return ((a < 0 && b > 0) || (a > 0 && b < 0)) ? true : false;
 	}
 
 	/* Return absolute value */
 	public static int abs(int a) {
-		if (a < 0) return FnNegate(a);
-		else return a;
+		if (a < 0)
+			return FnNegate(a);
+		else
+			return a;
 	}
 
 	/* Multiply a by b by adding a to itself b times */
 	public static int FnTimes(int a, int b) {
-		if (a < b) return FnTimes(b, a); // algo is faster if b < a
+		if (a < b)
+			return FnTimes(b, a); // algo is faster if b < a
 		int sum = 0;
-		for (int iter = abs(b); iter > 0; --iter) sum += a;
-		if (b < 0) sum = FnNegate(sum);
+		for (int iter = abs(b); iter > 0; --iter)
+			sum += a;
+		if (b < 0)
+			sum = FnNegate(sum);
 		return sum;
 	}
 
 	// returns 1, if a/b >= 0.5, and 0 otherwise
 	public static int DefineAndRoundFraction(int a, int b) {
-		if(FnTimes(abs(a), 2) >= abs(b)) return 1;
-		else return 0;
+		if (FnTimes(abs(a), 2) >= abs(b))
+			return 1;
+		else
+			return 0;
 	}
 
-	/* Divide a by b by literally counting how many times does b go into
-	 * a. That is, count how many times you can subtract b from a until
-	 * you hit 0. */
-	public static int FnDivide(int a, int b) throws java.lang.ArithmeticException {
+	/*
+	 * Divide a by b by literally counting how many times does b go into a. That
+	 * is, count how many times you can subtract b from a until you hit 0.
+	 */
+	public static int FnDivide(int a, int b)
+			throws java.lang.ArithmeticException {
 		if (b == 0) {
 			throw new java.lang.ArithmeticException("ERROR: Divide by zero.");
 		}
@@ -56,14 +66,19 @@ public class Question {
 		for (divend = abs(a); divend >= abs(divisor); divend += divisor) {
 			++quotient;
 		}
-		if (DifferentSigns(a, b)) quotient = FnNegate(quotient);
+		if (DifferentSigns(a, b))
+			quotient = FnNegate(quotient);
 		return quotient;
 	}
-	
+
 	public static int randomInt(int n) {
+		// int num = 0;
+		// while (num == 0)
+		// num = (int) (Math.random() * n * 2) - n;
+		// return num;
 		return (int) (Math.random() * n);
 	}
-	
+
 	public static void main(String[] args) {
 		for (int i = 0; i < 100; i++) {
 			int a = randomInt(10);
