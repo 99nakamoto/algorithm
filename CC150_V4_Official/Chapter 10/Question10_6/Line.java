ae1e4408423e78f032debc3008817518022cdb1d
Ran <ran@worksap.co.jp>
2014-09-08 15:56:11 +0800

    d

--- CC150_V4_Official/Chapter 10/Question10_6/Line.java
+++ CC150_V4_Official/Chapter 10/Question10_6/Line.java
@@ -1,10 +1,13 @@
 package Question10_6;
 
 public class Line {
+
 	private static double epsilon = .0001;
+
 	public double slope;
 	public double intercept;
 	private boolean infinite_slope = false;
+
 	public Line(GraphPoint p, GraphPoint q) {
 		if (Math.abs(p.x - q.x) > epsilon) { // if x’s are different
 			slope = (p.y - q.y) / (p.x - q.x); // compute slope
@@ -14,29 +17,29 @@ public class Line {
 			intercept = p.x; // x-intercept, since slope is infinite
 		}
 	}
-	
+
 	public boolean isEqual(double a, double b) {
 		return (Math.abs(a - b) < epsilon);
 	}
-	
+
 	public void Print() {
-		System.out.println("y = " + slope + "x + " + intercept);
+		System.out.println("slope = " + slope + "\nintercept = " + intercept);
 	}
-	
-    @Override  
-    public int hashCode()  {
-        int sl = (int)(slope * 1000);
-        int in = (int)(intercept * 1000);
-        return sl | in;
-    } 
-    
-    @Override  
-        public boolean equals(Object o) {  
+
+	@Override
+	public int hashCode() {
+		int sl = (int) (slope * 1000);
+		int in = (int) (intercept * 1000);
+		return sl | in;
+	}
+
+	@Override
+	public boolean equals(Object o) {
 		Line l = (Line) o;
-    	if (isEqual(l.slope, slope) && isEqual(l.intercept, intercept) 
-			&& (infinite_slope == l.infinite_slope)) {
-    		return true;
-    	}
-        return false;
-    }      
+		if (isEqual(l.slope, slope) && isEqual(l.intercept, intercept)
+				&& (infinite_slope == l.infinite_slope)) {
+			return true;
+		}
+		return false;
+	}
 }
