12e134c6f47f37349d43a07cf46ad85efaeab18d
Michael <theimmortalfish@gmail.com>
2014-09-14 02:18:22 +0800

    d

--- Gaylemcd_V5/Chapter 1/Question1_8/Question.java
+++ Gaylemcd_V5/Chapter 1/Question1_8/Question.java
@@ -1,6 +1,7 @@
 package Question1_8;
 
 public class Question {
+
 	public static boolean isSubstring(String big, String small) {
 		if (big.indexOf(small) >= 0) {
 			return true;
@@ -8,20 +9,26 @@ public class Question {
 			return false;
 		}
 	}
-	
+
 	public static boolean isRotation(String s1, String s2) {
-	    int len = s1.length();
-	    /* check that s1 and s2 are equal length and not empty */
-	    if (len == s2.length() && len > 0) { 
-	    	/* concatenate s1 and s1 within new buffer */
-	    	String s1s1 = s1 + s1;
-	    	return isSubstring(s1s1, s2);
-	    }
-	    return false;
+		int len = s1.length();
+		/* check that s1 and s2 are equal length and not empty */
+		if (len == s2.length() && len > 0) {
+			/* concatenate s1 and s1 within new buffer */
+			String s1s1 = s1 + s1;
+			return isSubstring(s1s1, s2);
+		}
+		return false;
 	}
-	
+
+	public static boolean isRotationMe(String s1, String s2) {
+		// need some special case checkings
+		return isSubstring(s1 + s1, s2);
+	}
+
 	public static void main(String[] args) {
-		String[][] pairs = {{"apple", "pleap"}, {"waterbottle", "erbottlewat"}, {"camera", "macera"}};
+		String[][] pairs = { { "apple", "pleap" },
+				{ "waterbottle", "erbottlewat" }, { "camera", "macera" } };
 		for (String[] pair : pairs) {
 			String word1 = pair[0];
 			String word2 = pair[1];
