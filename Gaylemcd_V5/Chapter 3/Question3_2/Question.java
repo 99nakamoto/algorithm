fbaa6c6464e259470db7ce2e7d94d2f8edea23ee
Ran <ran@worksap.co.jp>
2014-09-15 16:29:36 +0800

    hello world

--- Gaylemcd_V5/Chapter 3/Question3_2/Question.java
+++ Gaylemcd_V5/Chapter 3/Question3_2/Question.java
@@ -3,19 +3,24 @@ package Question3_2;
 import CtCILibrary.AssortedMethods;
 
 public class Question {
+
 	public static void main(String[] args) {
 		StackWithMin stack = new StackWithMin();
 		StackWithMin2 stack2 = new StackWithMin2();
+		StackMyAnswer stackMe = new StackMyAnswer();
 		for (int i = 0; i < 15; i++) {
 			int value = AssortedMethods.randomIntInRange(0, 100);
 			stack.push(value);
 			stack2.push(value);
+			stackMe.push(value);
 			System.out.print(value + ", ");
 		}
 		System.out.println('\n');
 		for (int i = 0; i < 15; i++) {
-			System.out.println("Popped " + stack.pop().value + ", " + stack2.pop());
-			System.out.println("New min is " + stack.min() + ", " + stack2.min());
+			System.out.println("Popped ..... " + stack.pop().value + ", "
+					+ stack2.pop() + ", " + stackMe.pop());
+			System.out.println("New min ................ " + stack.min() + ", "
+					+ stack2.min() + ", " + stackMe.min());
 		}
 	}
 
--- Gaylemcd_V5/Chapter 3/Question3_2/StackWithMin2.java
+++ Gaylemcd_V5/Chapter 3/Question3_2/StackWithMin2.java
@@ -3,27 +3,28 @@ package Question3_2;
 import java.util.Stack;
 
 public class StackWithMin2 extends Stack<Integer> {
+
 	Stack<Integer> s2;
-	
+
 	public StackWithMin2() {
-		s2 = new Stack<Integer>();		
+		s2 = new Stack<Integer>();
 	}
-	
-	public void push(int value){
+
+	public void push(int value) {
 		if (value <= min()) {
 			s2.push(value);
 		}
 		super.push(value);
 	}
-	
+
 	public Integer pop() {
 		int value = super.pop();
 		if (value == min()) {
-			s2.pop();			
+			s2.pop();
 		}
 		return value;
 	}
-	
+
 	public int min() {
 		if (s2.isEmpty()) {
 			return Integer.MAX_VALUE;
@@ -32,5 +33,3 @@ public class StackWithMin2 extends Stack<Integer> {
 		}
 	}
 }
-
-
