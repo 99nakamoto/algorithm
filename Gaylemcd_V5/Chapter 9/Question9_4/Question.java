a108d8386584c57f5355e3f36fe8f5c9b16ee21d
Michael <theimmortalfish@gmail.com>
2014-09-16 18:01:48 +0800

    d

--- Gaylemcd_V5/Chapter 9/Question9_4/Question.java
+++ Gaylemcd_V5/Chapter 9/Question9_4/Question.java
@@ -4,11 +4,12 @@ import java.util.*;
 
 public class Question {
 
-	public static ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index) {
+	public static ArrayList<ArrayList<Integer>> getSubsets(
+			ArrayList<Integer> set, int index) {
 		ArrayList<ArrayList<Integer>> allsubsets;
 		if (set.size() == index) { // Base case - add empty set
 			allsubsets = new ArrayList<ArrayList<Integer>>();
-			allsubsets.add(new ArrayList<Integer>()); 
+			allsubsets.add(new ArrayList<Integer>());
 		} else {
 			allsubsets = getSubsets(set, index + 1);
 			int item = set.get(index);
@@ -23,9 +24,10 @@ public class Question {
 		}
 		return allsubsets;
 	}
-	
-	public static ArrayList<Integer> convertIntToSet(int x, ArrayList<Integer> set) {
-		ArrayList<Integer> subset = new ArrayList<Integer>(); 
+
+	public static ArrayList<Integer> convertIntToSet(int x,
+			ArrayList<Integer> set) {
+		ArrayList<Integer> subset = new ArrayList<Integer>();
 		int index = 0;
 		for (int k = x; k > 0; k >>= 1) {
 			if ((k & 1) == 1) {
@@ -35,17 +37,18 @@ public class Question {
 		}
 		return subset;
 	}
-	
-	public static ArrayList<ArrayList<Integer>> getSubsets2(ArrayList<Integer> set) {
+
+	public static ArrayList<ArrayList<Integer>> getSubsets2(
+			ArrayList<Integer> set) {
 		ArrayList<ArrayList<Integer>> allsubsets = new ArrayList<ArrayList<Integer>>();
-		int max = 1 << set.size(); /* Compute 2^n */ 
+		int max = 1 << set.size(); /* Compute 2^n */
 		for (int k = 0; k < max; k++) {
 			ArrayList<Integer> subset = convertIntToSet(k, set);
 			allsubsets.add(subset);
 		}
 		return allsubsets;
 	}
-	
+
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < 3; i++) {
@@ -53,9 +56,9 @@ public class Question {
 		}
 		ArrayList<ArrayList<Integer>> subsets = getSubsets(list, 0);
 		System.out.println(subsets.toString());
-		
+
 		ArrayList<ArrayList<Integer>> subsets2 = getSubsets2(list);
-		System.out.println(subsets2.toString());		
+		System.out.println(subsets2.toString());
 	}
 
 }
