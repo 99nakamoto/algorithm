a24dbec433056ff9c1a9a9297a2e1a7dd79f6b74
Michael <theimmortalfish@gmail.com>
2014-09-16 13:01:59 +0800

    finished chap5

--- Gaylemcd_V5/Chapter 5/Question5_1/Question.java
+++ Gaylemcd_V5/Chapter 5/Question5_1/Question.java
@@ -9,32 +9,61 @@ public class Question {
 		if (i >= 32 || j < i) {
 			return 0;
 		}
-		
-		/* Create a mask to clear bits i through j in n
-		/* EXAMPLE: i = 2, j = 4. Result should be 11100011.
-		 * (Using 8 bits for this example.  This is obviously not actually 8 bits.)
+
+		/*
+		 * Create a mask to clear bits i through j in n /* EXAMPLE: i = 2, j =
+		 * 4. Result should be 11100011. (Using 8 bits for this example. This is
+		 * obviously not actually 8 bits.)
 		 */
 		int allOnes = ~0; // allOnes = 11111111
-		
-		int left = allOnes << (j + 1); // 1s through position j, then 0s. left = 11100000	
-	  	int right = ((1 << i) - 1); // 1’s after position i.  right = 00000011
-		int mask = left | right; // All 1s, except for 0s between i and j. mask = 11100011
+
+		int left = allOnes << (j + 1); // 1s through position j, then 0s. left =
+										// 11100000
+		int right = ((1 << i) - 1); // 1’s after position i. right = 00000011
+		int mask = left | right; // All 1s, except for 0s between i and j. mask
+									// = 11100011
 
 		/* Clear i through j, then put m in there */
 		int n_cleared = n & mask; // Clear bits j through i.
 		int m_shifted = m << i; // Move m into correct position.
-		
+
 		/* OR them, and we're done! */
-		return n_cleared | m_shifted; 
+		return n_cleared | m_shifted;
 	}
-	
+
+	public static int myAnswer(int n, int m, int i, int j) {
+		int rr = (~0 >>> (31 - j));
+		int ll = (~0 << i);
+		// printBinary(rr);
+		// printBinary(ll);
+
+		int middleMask = ll & rr;
+		int twoEndMask = ll ^ rr;
+
+		n = n & twoEndMask;
+		m = (m << i) & middleMask;
+
+		return n | m;
+	}
+
 	public static void main(String[] args) {
+		
+		System.out.println("Input: ");
 		int a = 103217;
-		System.out.println(AssortedMethods.toFullBinaryString(a));
+		printBinary(a);
 		int b = 13;
-		System.out.println(AssortedMethods.toFullBinaryString(b));		
-		int c = updateBits(a, b, 4, 12);
-		System.out.println(AssortedMethods.toFullBinaryString(c));
+		printBinary(b);
+
+		int result = updateBits(a, b, 4, 12);
+		System.out.println("Expected: ");
+		printBinary(result);
+		result = myAnswer(a, b, 4, 12);
+		System.out.println("My Answer: ");
+		printBinary(result);
+	}
+
+	private static void printBinary(int num) {
+		System.out.println(AssortedMethods.toFullBinaryString(num));
 	}
 
 }
--- Gaylemcd_V5/Chapter 5/Question5_5/Question.java
+++ Gaylemcd_V5/Chapter 5/Question5_5/Question.java
@@ -5,27 +5,54 @@ import CtCILibrary.AssortedMethods;
 public class Question {
 	public static int bitSwapRequired(int a, int b) {
 		int count = 0;
-		for (int c = a ^ b; c != 0; c = c >> 1) { 
+		for (int c = a ^ b; c != 0; c = c >> 1) {
 			count += c & 1;
 		}
 		return count;
 	}
-	
-	public static int bitSwapRequired2(int a, int b){
+
+	public static int bitSwapRequired2(int a, int b) {
 		int count = 0;
-		for (int c = a ^ b; c != 0; c = c & (c-1)) {
+		for (int c = a ^ b; c != 0; c = c & (c - 1)) {
 			count++;
 		}
 		return count;
 	}
-	
+
+	public static int calcBitsSwapMe1(int a, int b) {
+		int num = a ^ b;
+		int count = 0;
+		while (num != 0) {
+			count += num & 1;
+			num = num >>> 1;
+		}
+		return count;
+	}
+
+	public static int calcBitsSwapMe2(int a, int b) {
+		int num = a ^ b;
+		int count = 0;
+		while (num != 0) {
+			num = num & (num - 1);
+			count++;
+		}
+		return count;
+	}
+
 	public static void main(String[] args) {
+
 		int a = 23432;
 		int b = 512132;
 		System.out.println(a + ": " + AssortedMethods.toFullBinaryString(a));
 		System.out.println(b + ": " + AssortedMethods.toFullBinaryString(b));
+
 		int nbits = bitSwapRequired(a, b);
 		int nbits2 = bitSwapRequired2(a, b);
 		System.out.println("Required number of bits: " + nbits + " " + nbits2);
+
+		int ans1 = calcBitsSwapMe1(a, b);
+		int ans2 = calcBitsSwapMe2(a, b);
+		System.out.println("My answer is: " + ans1 + " " + ans2);
+
 	}
 }
