f6bbe0e0ccefcfa55b3f4817896805a4381e2cd3
Ran <ran@worksap.co.jp>
2014-09-16 17:56:25 +0800

    d

--- Gaylemcd_V5/Chapter 9/Question9_3/Question.java
+++ Gaylemcd_V5/Chapter 9/Question9_3/Question.java
@@ -14,7 +14,7 @@ public class Question {
 		}
 		return -1;
 	}
-	
+
 	public static int magicFast(int[] array, int start, int end) {
 		if (end < start || start < 0 || end >= array.length) {
 			return -1;
@@ -22,45 +22,51 @@ public class Question {
 		int mid = (start + end) / 2;
 		if (array[mid] == mid) {
 			return mid;
-		} else if (array[mid] > mid){
+		} else if (array[mid] > mid) {
 			return magicFast(array, start, mid - 1);
 		} else {
 			return magicFast(array, mid + 1, end);
 		}
 	}
-	
+
 	public static int magicFast(int[] array) {
 		return magicFast(array, 0, array.length - 1);
 	}
-	
+
 	/* Creates an array that is distinct and sorted */
 	public static int[] getDistinctSortedArray(int size) {
 		int[] array = AssortedMethods.randomArray(size, -1 * size, size);
 		Arrays.sort(array);
 		for (int i = 1; i < array.length; i++) {
-			if (array[i] == array[i-1]) {
+			if (array[i] == array[i - 1]) {
 				array[i]++;
 			} else if (array[i] < array[i - 1]) {
-				array[i] = array[i-1] + 1;
+				array[i] = array[i - 1] + 1;
 			}
 		}
 		return array;
 	}
-	
+
 	public static void main(String[] args) {
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100; i++) {
 			int size = AssortedMethods.randomIntInRange(5, 20);
 			int[] array = getDistinctSortedArray(size);
 			int v2 = magicFast(array);
-			if (v2 == -1 && magicSlow(array) != -1) {
+			if (magicSlow(array) == -1) {
+				// do nothing
+			} else if (v2 == -1 && magicSlow(array) != -1) {
 				int v1 = magicSlow(array);
-				System.out.println("Incorrect value: index = -1, actual = " + v1 + " " + i);
+				System.out.println("Incorrect value: index = -1, actual = "
+						+ v1 + " " + i);
 				System.out.println(AssortedMethods.arrayToString(array));
 				break;
 			} else if (v2 > -1 && array[v2] != v2) {
-				System.out.println("Incorrect values: index= " + v2 + ", value " + array[v2]);
+				System.out.println("Incorrect values: index= " + v2
+						+ ", value " + array[v2]);
 				System.out.println(AssortedMethods.arrayToString(array));
 				break;
+			} else {
+				System.out.println("Found index = " + v2);
 			}
 		}
 	}
