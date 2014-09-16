692b34c586ce9f2fd97f3687527dc9c921a53218
Ran <ran@worksap.co.jp>
2014-09-16 18:01:48 +0800

    d

--- Gaylemcd_V5/Chapter 9/Question9_7/Question.java
+++ Gaylemcd_V5/Chapter 9/Question9_7/Question.java
@@ -5,9 +5,9 @@ public class Question {
 	public enum Color {
 		Black, White, Red, Yellow, Green
 	}
-	
+
 	public static String PrintColor(Color c) {
-		switch(c) {
+		switch (c) {
 		case Black:
 			return "B";
 		case White:
@@ -21,7 +21,7 @@ public class Question {
 		}
 		return "X";
 	}
-	
+
 	public static void PrintScreen(Color[][] screen) {
 		for (int i = 0; i < screen.length; i++) {
 			for (int j = 0; j < screen[0].length; j++) {
@@ -30,12 +30,13 @@ public class Question {
 			System.out.println();
 		}
 	}
-	
+
 	public static int randomInt(int n) {
 		return (int) (Math.random() * n);
 	}
-	
-	public static boolean PaintFill(Color[][] screen, int x, int y, Color ocolor, Color ncolor) {
+
+	public static boolean PaintFill(Color[][] screen, int x, int y,
+			Color ocolor, Color ncolor) {
 		if (x < 0 || x >= screen[0].length || y < 0 || y >= screen.length) {
 			return false;
 		}
@@ -48,19 +49,20 @@ public class Question {
 		}
 		return true;
 	}
-	
+
 	public static boolean PaintFill(Color[][] screen, int x, int y, Color ncolor) {
-		if (screen[y][x] == ncolor) return false;
+		if (screen[y][x] == ncolor)
+			return false;
 		return PaintFill(screen, x, y, screen[y][x], ncolor);
 	}
-	
+
 	public static void main(String[] args) {
 		int N = 10;
 		Color[][] screen = new Color[N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				screen[i][j] = Color.Black;
-			}			
+			}
 		}
 		for (int i = 0; i < 100; i++) {
 			screen[randomInt(N)][randomInt(N)] = Color.Green;
