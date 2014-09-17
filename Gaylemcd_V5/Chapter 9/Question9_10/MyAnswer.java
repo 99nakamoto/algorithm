f447609ad3af0a2cf4064ecdfa30bae2e9588d0a
Ran <ran@worksap.co.jp>
2014-09-17 12:30:11 +0800

    d

---
+++ Gaylemcd_V5/Chapter 9/Question9_10/MyAnswer.java
@@ -0,0 +1,11 @@
+package Question9_10;
+
+import java.util.ArrayList;
+
+public class MyAnswer {
+
+	public static ArrayList<Box> createStack(Box[] boxes) {
+		ArrayList<Box> ans = new ArrayList<Box>();
+		return ans;
+	}
+}
