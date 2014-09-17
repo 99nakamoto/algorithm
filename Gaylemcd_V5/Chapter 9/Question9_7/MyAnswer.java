e21185facc09bcc91fc4b61e9a08d768fa6bc389
Ran <ran@worksap.co.jp>
2014-09-17 11:45:45 +0800

    d

--- Gaylemcd_V5/Chapter 9/Question9_7/MyAnswer.java
+++ Gaylemcd_V5/Chapter 9/Question9_7/MyAnswer.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 
 public class MyAnswer {
 
-	public static void PaintFill(Color[][] screen, int posX, int posY,
+	public static void PaintFill1(Color[][] screen, int posX, int posY,
 			Color ncolor) {
 		// the queue keeps the list of positions that I'm going to visit
 		Queue<Position> q = new LinkedList<Position>();
@@ -37,7 +37,31 @@ public class MyAnswer {
 				}
 			}
 		}
+	}
 
+	public static void PaintFill2(Color[][] screen, int posX, int posY,
+			Color ncolor) {
+		// the queue keeps the list of positions that I'm going to visit
+		Queue<Position> q = new LinkedList<Position>();
+		int len = screen.length;
+		Color original = screen[posX][posY];
+		// visited origin node first
+		q.offer(new Position(posX, posY));
+		while (!q.isEmpty()) {
+			// visit positions in q one by one (mark color as 'Temp')
+			Position p = q.poll();
+			if (p.x < 0 || p.x >= len || p.y < 0 || p.y >= len) {
+				// invalid pos coordinate
+				continue;
+			} else if (screen[p.x][p.y] != original) {
+				continue;
+			}
+			screen[p.x][p.y] = ncolor;
+			q.offer(new Position(p.x - 1, p.y));
+			q.offer(new Position(p.x + 1, p.y));
+			q.offer(new Position(p.x, p.y - 1));
+			q.offer(new Position(p.x, p.y + 1));
+		}
 	}
 
 	static class Position {
