399a6ec8bf371a28e9a32056a162cefd572bb860
Michael <theimmortalfish@gmail.com>
2014-09-18 10:43:23 +0800

    d

--- Gaylemcd_V5/Chapter 9/Question9_11/MyAnswer.java
+++ Gaylemcd_V5/Chapter 9/Question9_11/MyAnswer.java
@@ -3,35 +3,34 @@ package Question9_11;
 public class MyAnswer {
 
 	public static int countMyAnswer(String exp, boolean result) {
+		if (exp.length() == 1) {
+			return convertIntToBool(exp.charAt(0)) == result ? 1 : 0;
+		}
 		// eg. 1^0|0|1
 		// result: true
 		int totalCount = 0;
-		if (exp.length() == 1) {
-			if (convertIntToBool(exp.charAt(0)) == result) {
-				return 1;
-			} else {
-				return 0;
+		for (int i = 1; i < exp.length(); i += 2) {
+
+			char op = exp.charAt(i);
+			String firstHalf = exp.substring(0, i);
+			String secondHalf = exp.substring(i + 1);
+
+			int firstHalfTrue = countMyAnswer(firstHalf, true);
+			int firstHalfFalse = countMyAnswer(firstHalf, false);
+			int secondHalfTrue = countMyAnswer(secondHalf, true);
+			int secondHalfFalse = countMyAnswer(secondHalf, false);
+
+			if (evaluate('0', op, '0') == result) {
+				totalCount += firstHalfFalse * secondHalfFalse;
 			}
-		} else {
-			for (int i = 1; i < exp.length(); i += 2) {
-				char op = exp.charAt(i);
-				String firstHalf = exp.substring(0, i);
-				String secondHalf = exp.substring(i + 1);
-
-				int firstHalfTrue = countMyAnswer(firstHalf, true);
-				int firstHalfFalse = countMyAnswer(firstHalf, false);
-				int secondHalfTrue = countMyAnswer(secondHalf, true);
-				int secondHalfFalse = countMyAnswer(secondHalf, false);
-
-				if (evaluate('0', op, '0') == result) {
-					totalCount += firstHalfFalse * secondHalfFalse;
-				} else if (evaluate('0', op, '1') == result) {
-					totalCount += firstHalfFalse * secondHalfTrue;
-				} else if (evaluate('1', op, '0') == result) {
-					totalCount += firstHalfTrue * secondHalfFalse;
-				} else if (evaluate('1', op, '1') == result) {
-					totalCount += firstHalfTrue * secondHalfTrue;
-				}
+			if (evaluate('0', op, '1') == result) {
+				totalCount += firstHalfFalse * secondHalfTrue;
+			}
+			if (evaluate('1', op, '0') == result) {
+				totalCount += firstHalfTrue * secondHalfFalse;
+			}
+			if (evaluate('1', op, '1') == result) {
+				totalCount += firstHalfTrue * secondHalfTrue;
 			}
 		}
 		return totalCount;
@@ -42,14 +41,6 @@ public class MyAnswer {
 			return true;
 		} else {
 			return false;
-		}
-	}
-
-	private static int convertBoolToInt(boolean b) {
-		if (b == true) {
-			return 1;
-		} else {
-			return 0;
 		}
 	}
 
