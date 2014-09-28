3f33dbcb19d387f278268667e5678bbe14d4db3e
Home Mac <codeaperan@gmail.com>
2014-09-28 22:33:07 +0800

    d

--- Gaylemcd_V5/Chapter 14/Question14_6/Question.java
+++ Gaylemcd_V5/Chapter 14/Question14_6/Question.java
@@ -7,22 +7,36 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		int size = 10;
-		CircularArray<String> array = new CircularArray<String>(size);
+		CircularArray<String> array1 = new CircularArray<String>(size);
+		MyCircularArray<String> array2 = new MyCircularArray<String>(size);
 		for (int i = 0; i < size; i++) {
-			array.set(i, String.valueOf(i));
+			array1.set(i, String.valueOf(i));
+			array2.put(String.valueOf(i));
 		}
-		
-		array.rotate(3);
+
+		array1.rotate(3);
+		array2.rotate(3);
+		System.out.println("Rotate by 3: ");
 		for (int i = 0; i < size; i++) {
-			System.out.println(array.get(i));
+			System.out.print(" * " + array1.get(i));
 		}
-		
-		System.out.println("");
-		
-		array.rotate(2);
-		for (String s : array) {
-			System.out.println(s);
+		System.out.println();
+		for (int i = 0; i < size; i++) {
+			System.out.print(" * " + array2.get(i));
 		}
+		System.out.println();
+
+		array1.rotate(2);
+		array2.rotate(2);
+		System.out.println("Rotate by another 2: ");
+		for (String s : array1) {
+			System.out.print(" - " + s);
+		}
+		System.out.println();
+		for (String s : array2) {
+			System.out.print(" - " + s);
+		}
+		System.out.println();
 	}
 
 }
