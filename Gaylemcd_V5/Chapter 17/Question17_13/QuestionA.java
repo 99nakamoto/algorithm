3c560899d6f2ed17376363c570c4eb37272cd5da
Ran <ran@worksap.co.jp>
2014-09-29 19:59:59 +0800

    d

--- Gaylemcd_V5/Chapter 17/Question17_13/QuestionA.java
+++ Gaylemcd_V5/Chapter 17/Question17_13/QuestionA.java
@@ -1,35 +1,37 @@
 package Question17_13;
 
 public class QuestionA {
+
 	private static class NodePair {
 		BiNode head;
 		BiNode tail;
 
-		public NodePair(BiNode head, BiNode tail)  {
+		public NodePair(BiNode head, BiNode tail) {
 			this.head = head;
 			this.tail = tail;
 		}
 	}
-	
+
 	public static NodePair convert(BiNode root) {
 		if (root == null) {
 			return null;
 		}
-		
+
 		NodePair part1 = convert(root.node1);
 		NodePair part2 = convert(root.node2);
-		
+
 		if (part1 != null) {
 			concat(part1.tail, root);
 		}
-		
+
 		if (part2 != null) {
 			concat(root, part2.head);
 		}
-		
-		return new NodePair(part1 == null ? root : part1.head, part2 == null ? root : part2.tail);
-	}	
-	
+
+		return new NodePair(part1 == null ? root : part1.head,
+				part2 == null ? root : part2.tail);
+	}
+
 	public static void concat(BiNode x, BiNode y) {
 		x.node2 = y;
 		y.node1 = x;
