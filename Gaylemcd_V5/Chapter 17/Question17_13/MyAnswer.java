c998b860bda2a051e9cc08d0c7dd34d6919b079b
Ran <ran@worksap.co.jp>
2014-09-30 16:00:31 +0800

    17.13

--- Gaylemcd_V5/Chapter 17/Question17_13/MyAnswer.java
+++ Gaylemcd_V5/Chapter 17/Question17_13/MyAnswer.java
@@ -3,7 +3,37 @@ package Question17_13;
 public class MyAnswer {
 
 	public static BiNode convert(BiNode root) {
-		return root;
+		if (root == null) {
+			return null;
+		}
+		return helper(root);
+	}
+
+	private static BiNode helper(BiNode node) {
+		// node is not null
+		BiNode newHead = node;
+		// do left part
+		if (node.node1 != null) {
+			newHead = helper(node.node1);
+			BiNode leftTail = getListTail(newHead);
+			leftTail.node2 = node;
+			node.node1 = leftTail;
+		}
+		// do right part
+		if (node.node2 != null) {
+			BiNode rightHead = helper(node.node2);
+			node.node2 = rightHead;
+			rightHead.node1 = node;
+		}
+		return newHead;
+	}
+
+	private static BiNode getListTail(BiNode head) {
+		// head is not null
+		while (head.node2 != null) {
+			head = head.node2;
+		}
+		return head;
 	}
 
 	public static void main(String[] args) {
