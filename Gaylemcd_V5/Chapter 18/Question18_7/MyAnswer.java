19006bf5b887827f52fd18339f36d5df46afbbaa
Michael <theimmortalfish@gmail.com>
2014-10-01 19:10:47 +0800

    Signed-off-by: Ran <ran@worksap.co.jp>

--- Gaylemcd_V5/Chapter 18/Question18_7/MyAnswer.java
+++ Gaylemcd_V5/Chapter 18/Question18_7/MyAnswer.java
@@ -1,11 +1,17 @@
 package Question18_7;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MyAnswer {
 
 	public static void printLongestWord(String[] arr) {
 		Arrays.sort(arr, new LengthComparator());
+		List<String> list = new ArrayList<String>(); 
+		for (String str: arr) {
+			list.add(str);
+		}
 		for (String word: arr) {
 			if (canDivide(word, arr)) {
 				System.out.println(word);
@@ -16,7 +22,18 @@ public class MyAnswer {
 	}
 
 	private static boolean canDivide(String word, String[] arr) {
+		
+		return false;
+	}
 
+	private static boolean helper(String word, int pos, int len, String[] arr) {
+		if (pos == len) {
+			return true;
+		}
+		for (int i = pos + 1; i <= len; i++) {
+			String str = word.substring(pos, i);
+			if (arr.)
+		}
 		return false;
 	}
 }
