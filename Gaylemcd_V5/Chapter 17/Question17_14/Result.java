8cda305220a00cc4ac5ebd3d61e508173fb5ce5e
Ran <ran@worksap.co.jp>
2014-10-01 14:19:19 +0800

    a

--- Gaylemcd_V5/Chapter 17/Question17_14/Result.java
+++ Gaylemcd_V5/Chapter 17/Question17_14/Result.java
@@ -1,5 +1,9 @@
 package Question17_14;
 
+import java.util.Hashtable;
+
+import CtCILibrary.Trie;
+
 public class Result {
 
 	public int invalid = Integer.MAX_VALUE;
@@ -22,5 +26,43 @@ public class Result {
 		}
 
 		return r2.invalid < r1.invalid ? r2 : r1;
+	}
+
+	/* incomplete code */
+	public static Result parse(String sentence, Trie dictionary, int wordStart,
+			int wordEnd, Hashtable<Integer, Result> cache) {
+		if (wordEnd >= sentence.length()) {
+			return new Result(wordEnd - wordStart, sentence
+					.substring(wordStart).toUpperCase());
+		}
+		if (cache.containsKey(wordStart)) {
+			return cache.get(wordStart).clone();
+		}
+		String currentWord = sentence.substring(wordStart, wordEnd + 1);
+		boolean validPartial = dictionary.contains(currentWord, false);
+		boolean validExact = validPartial
+				&& dictionary.contains(currentWord, true);
+
+		/* break current word */
+		Result bestExact = parse(sentence, dictionary, wordEnd + 1,
+				wordEnd + 1, cache);
+		if (validExact) {
+			bestExact.parsed = currentWord + " " + bestExact.parsed;
+		} else {
+			bestExact.invalid += currentWord.length();
+			bestExact.parsed = currentWord.toUpperCase() + " "
+					+ bestExact.parsed;
+		}
+
+		/* extend current word */
+		Result bestExtend = null;
+		if (validPartial) {
+			bestExtend = parse(sentence, dictionary, wordStart, wordEnd + 1, cache);
+		}
+
+		/* find best */
+		Result best = Result.min(bestExact, bestExtend);
+		cache.put(wordStart, best.clone());
+		return best;
 	}
 }
