2fc60cb23524ae04237515f3e1873e5dbf847b5b
Michael <theimmortalfish@gmail.com>
2014-11-05 19:39:27 +0800

    Signed-off-by: Ran <ran@worksap.co.jp>

--- CodeApe/src/Solution.java
+++ CodeApe/src/Solution.java
@@ -1,73 +1,29 @@
+import common.ListNode;
+
+//Reverse a linked list from position m to n. Do it in-place and in one-pass.
+//
+//For example:
+//Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+//
+//return 1->4->3->2->5->NULL.
+//
+//Note:
+//Given m, n satisfy the following condition:
+//1 ≤ m ≤ n ≤ length of list.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
-	public String addBinary(String a, String b) {
-		if (a == null || b == null) {
-			return "";
-		}
-		int m = a.length();
-		int n = b.length();
-		int total = Math.max(m, n) + 1;
-		int[] ans = new int[total];
-
-		return null;
-	}
-
-	public static void main(String[] args) {
-		System.out
-				.println("SELECT "
-						+ "F.SUMMARIZE_TOTAL, "
-						+ "F.COUNT_TO_SUMMARIZE,"
-						+ "F.ACCOUNTING_TITLE,F.RELATION,F.TERM_ID, "
-						+ "S.CUSTOMER_NAME, "
-						+ "S.CUSTOMER_ADDRESS, "
-						+ "S.TRANSACTION_DETAIL "
-						+ "FROM ("
-						+ "SELECT "
-						+ "SUM(ENDING_BALANCE) SUMMARIZE_TOTAL, "
-						+ "COUNT(*) COUNT_TO_SUMMARIZE,"
-						+ "ACCOUNTING_TITLE, "
-						+ "RELATION, "
-						+ "TERM_ID "
-						+ "FROM "
-						+ "AC_AD_FORM004A "
-						+ "WHERE "
-						+ "CORP_ID = ? "
-						+ "AND ENDING_BALANCE < ? "
-						+ " AND RELATION <> 0 "
-						+ "AND IS_SUMMARIZED_RECORD = 0 "
-						+ "AND TERM_ID IN (?,?) "
-						+ "GROUP BY ACCOUNTING_TITLE, RELATION, TERM_ID "
-						+ "ORDER BY ACCOUNTING_TITLE, RELATION, TERM_ID "
-						+ ") F "
-						+ "LEFT JOIN  AC_AD_FORM004A_SUMMARIZED_MST S "
-						+ "ON F.ACCOUNTING_TITLE = S.ACCOUNTING_TITLE AND F.RELATION = S.RELATION AND S.CORP_ID = ?");
-
-		System.out
-				.println("SELECT "
-						+ "F.SUMMARIZE_TOTAL, "
-						+ "F.COUNT_TO_SUMMARIZE,"
-						+ "F.ACCOUNTING_TITLE,F.TERM_ID, "
-						+ "S.CUSTOMER_NAME, "
-						+ "S.CUSTOMER_ADDRESS, "
-						+ "S.MEMO "
-						+ "FROM ("
-						+ "SELECT "
-						+ "SUM(ENDING_BALANCE) SUMMARIZE_TOTAL, "
-						+ "COUNT(*) COUNT_TO_SUMMARIZE,"
-						+ "ACCOUNTING_TITLE, "
-						+ "TERM_ID "
-						+ "FROM "
-						+ "AC_AD_FORM009A "
-						+ "WHERE "
-						+ "CORP_ID = ? "
-						+ "AND ENDING_BALANCE < ? "
-						+ "AND IS_SUMMARIZED_RECORD = 0 "
-						+ "AND TERM_ID IN (?,?) "
-						+ "GROUP BY ACCOUNTING_TITLE, TERM_ID "
-						+ "ORDER BY ACCOUNTING_TITLE, TERM_ID "
-						+ ") F "
-						+ "LEFT JOIN  AC_AD_FORM009A_SUMMARIZED_MST S "
-						+ "ON F.ACCOUNTING_TITLE = S.ACCOUNTING_TITLE AND S.CORP_ID = ?"
-
-						+ "");
-	}
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        return null;
+    }
 }
