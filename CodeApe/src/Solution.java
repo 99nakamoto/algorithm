82c31f135d03c7d704dbab431c340efdf80c383b
Ran <ran@worksap.co.jp>
2014-12-08 20:37:10 +0800

    Signed-off-by: Ran <ran@worksap.co.jp>

--- CodeApe/src/Solution.java
+++ CodeApe/src/Solution.java
@@ -1,29 +1,54 @@
-import common.ListNode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-//Reverse a linked list from position m to n. Do it in-place and in one-pass.
-//
-//For example:
-//Given 1->2->3->4->5->NULL, m = 2 and n = 4,
-//
-//return 1->4->3->2->5->NULL.
-//
-//Note:
-//Given m, n satisfy the following condition:
-//1 ≤ m ≤ n ≤ length of list.
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        return null;
-    }
+
+	public static void main(String[] args) {
+		int[] num = new int[] { 0, 0, 0, 0 };
+		threeSum(num);
+	}
+
+	public static List<List<Integer>> threeSum(int[] num) {
+		List<List<Integer>> ans = new ArrayList<List<Integer>>();
+		if (num == null || num.length < 3) {
+			return ans;
+		}
+		Arrays.sort(num);
+		int len = num.length;
+		int left, right;
+		for (int i = 0; i < len; i++) {
+			left = i + 1;
+			right = len - 1;
+			while (left < right) {
+				int sum = num[i] + num[left] + num[right];
+				if (sum == 0) {
+					// now one triplet is found, add it to ans list
+					List<Integer> ls = new ArrayList<Integer>();
+					ls.add(num[i]);
+					ls.add(num[left]);
+					ls.add(num[right]);
+					ans.add(ls);
+				}
+				// shrink the range between left and right pointer
+				// (until the 2 pointers met)
+				if (sum >= 0) {
+					// move right pointer to the left
+					right--;
+					while (right >= 0 && num[right] == num[right + 1]) {
+						right--;
+					}
+				}
+				if (sum <= 0) {
+					// move left pointer to the right
+					left++;
+					while (left < len && num[left] == num[left - 1]) {
+						left++;
+					}
+				}
+			}
+		}
+		return ans;
+	}
+
 }
