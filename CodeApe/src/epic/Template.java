85122ba6985bc3057e94a599fa881c507fd53993
Ran <ran@worksap.co.jp>
2014-12-10 20:48:38 +0800

    d

---
+++ CodeApe/src/epic/Template.java
@@ -0,0 +1,14 @@
+package epic;
+
+public class Template {
+
+    public static void main(String[] args) {
+        Template ins = new Template();
+        System.out.println();
+    }
+
+    private void solution() {
+
+    }
+
+}
---
+++ CodeApe/src/epic/WellOrderedInt.java
@@ -0,0 +1,32 @@
+package epic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import common.Common;
+
+public class WellOrderedInt {
+
+    public static void main(String[] args) {
+        WellOrderedInt ins = new WellOrderedInt();
+        Common.printList(ins.wellOrdered(2));
+        System.out.println();
+    }
+
+    private List<Integer> wellOrdered(int length) {
+        List<Integer> ans = new ArrayList<Integer>();
+        helper(ans, 0, 0, length);
+        return ans;
+    }
+
+    private void helper(List<Integer> ans, int current, int start, int len) {
+        if (len == 0) {
+            ans.add(current);
+            return;
+        }
+        for (int i = start; i <= 9; i++) {
+            helper(ans, current * 10 + i, i + 1, len - 1);
+        }
+    }
+
+}
