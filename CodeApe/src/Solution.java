8b3cb7885f5f489eeb16ba753d9914759829f9c6
Michael <theimmortalfish@gmail.com>
2014-12-18 21:16:45 +0800

    d

--- CodeApe/src/Solution.java
+++ CodeApe/src/Solution.java
@@ -1,54 +1,51 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Solution {
 
 	public static void main(String[] args) {
-		int[] num = new int[] { 0, 0, 0, 0 };
-		threeSum(num);
+		Solution ins = new Solution();
+		int ans = ins.divide(-2147483648, -1);
+		System.out.println(ans);
 	}
 
-	public static List<List<Integer>> threeSum(int[] num) {
-		List<List<Integer>> ans = new ArrayList<List<Integer>>();
-		if (num == null || num.length < 3) {
-			return ans;
+	public int divide(int dividend, int divisor) {
+		int sign = 1;
+		long x = dividend;
+		long y = divisor;
+		if (x < 0) {
+			x = 0 - x;
+			sign *= -1;
 		}
-		Arrays.sort(num);
-		int len = num.length;
-		int left, right;
-		for (int i = 0; i < len; i++) {
-			left = i + 1;
-			right = len - 1;
-			while (left < right) {
-				int sum = num[i] + num[left] + num[right];
-				if (sum == 0) {
-					// now one triplet is found, add it to ans list
-					List<Integer> ls = new ArrayList<Integer>();
-					ls.add(num[i]);
-					ls.add(num[left]);
-					ls.add(num[right]);
-					ans.add(ls);
-				}
-				// shrink the range between left and right pointer
-				// (until the 2 pointers met)
-				if (sum >= 0) {
-					// move right pointer to the left
-					right--;
-					while (right >= 0 && num[right] == num[right + 1]) {
-						right--;
-					}
-				}
-				if (sum <= 0) {
-					// move left pointer to the right
-					left++;
-					while (left < len && num[left] == num[left - 1]) {
-						left++;
-					}
-				}
+		if (y < 0) {
+			y = 0 - y;
+			sign *= -1;
+		}
+		return divide(sign, x, y);
+	}
+
+	private int divide(int sign, long x, long y) {
+		// both x and y are positive numbers
+		if (x < y) {
+			return 0;
+		}
+		long quotient = 0;
+		long partialQuotient;
+		long partialSubtract;
+		while (x >= y) {
+			// the idea is to subtract a certain times of x from y
+			// and save the remainder value back to x
+			partialQuotient = 1;
+			partialSubtract = y;
+			while ((partialSubtract << 1) <= x) {
+				partialQuotient <<= 1; // doubles quotient
+				partialSubtract <<= 1; // doubles subtraction
 			}
+			x -= partialSubtract;
+			quotient += partialQuotient;
 		}
-		return ans;
+		long finalQuo = sign * quotient;
+		if (finalQuo < Integer.MIN_VALUE || finalQuo > Integer.MAX_VALUE) {
+			return Integer.MAX_VALUE;
+		} else {
+			return (int) finalQuo;
+		}
 	}
-
 }
