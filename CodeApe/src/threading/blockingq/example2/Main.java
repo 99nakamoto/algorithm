b500069e0ce1ee7cc3bfb4a20ce438fc4578e00d
abc <abc@abc.com>
2015-01-12 19:03:15 +0800

    a

--- CodeApe/src/threading/blockingq/example2/Main.java
+++ CodeApe/src/threading/blockingq/example2/Main.java
@@ -1,13 +1,10 @@
 package threading.blockingq.example2;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-
-public class BlockingQueueExample {
+public class Main {
 
 	public static void main(String[] args) throws Exception {
 
-		BlockingQueue queue = new ArrayBlockingQueue(1024);
+		MyBlockingQueue queue = new MyBlockingQueue(1024);
 
 		Producer producer = new Producer(queue);
 		Consumer consumer = new Consumer(queue);
