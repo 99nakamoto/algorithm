9753df22af78c4bf563939491ec28114bb6272c5
abc <abc@abc.com>
2015-01-21 00:45:30 +0800

    d

--- CodeApe/src/Solution.java
+++ CodeApe/src/Solution.java
@@ -1,28 +1,12 @@
-import java.util.LinkedList;
 
 public class Solution {
 
-	public static boolean isPrime(long s) {
-		for (long i = 2; i < s; i++) {
-			if ((s % i) == 0)
-				return false;
-		}
-		return true;
-	}
-
 	public static void main(String[] args) {
-		Solution p = new Solution();
-		LinkedList<Long> arr = new LinkedList<Long>();
-		for (long j = 2; j <= 999999; j++) {
-
-			if (isPrime(j))
-				arr.add(j);
-
-		}
-		// System.out.println("List of Prime Number are: "+ arr);
-		long t = arr.get(10001);
-
-		System.out.println("The Prime Number At 10001st position: " + t);
+		Solution ins = new Solution();
+		System.out.println();
 	}
 
+	public void solve(int A) {
+
+	}
 }
