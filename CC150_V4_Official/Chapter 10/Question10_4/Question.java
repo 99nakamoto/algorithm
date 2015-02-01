b3745ed91bb7494111a719e9eeba0fd517e971a7
abc <abc@abc.com>
2015-02-01 20:07:49 +0800

    d

--- CC150_V4_Official/Chapter 10/Question10_4/Question.java
+++ CC150_V4_Official/Chapter 10/Question10_4/Question.java
@@ -3,7 +3,7 @@ package Question10_4;
 public class Question {
 
 	/* Flip a positive sign to negative, or a negative sign to pos */
-	public static int FnNegate(int a) {
+	public static int negate(int a) {
 		int neg = 0;
 		int d = a < 0 ? 1 : -1;
 		while (a != 0) {
@@ -14,60 +14,59 @@ public class Question {
 	}
 
 	/* Subtract two numbers by negating b and adding them */
-	public static int FnMinus(int a, int b) {
-		return a + FnNegate(b);
+	public static int minus(int a, int b) {
+		return a + negate(b);
 	}
 
 	/* Check if a and b are different signs */
-	public static boolean DifferentSigns(int a, int b) {
+	public static boolean diffSign(int a, int b) {
 		return ((a < 0 && b > 0) || (a > 0 && b < 0)) ? true : false;
 	}
 
 	/* Return absolute value */
 	public static int abs(int a) {
 		if (a < 0)
-			return FnNegate(a);
+			return negate(a);
 		else
 			return a;
 	}
 
 	/* Multiply a by b by adding a to itself b times */
-	public static int FnTimes(int a, int b) {
+	public static int multiply(int a, int b) {
 		if (a < b)
-			return FnTimes(b, a); // algo is faster if b < a
+			return multiply(b, a); // algo is faster if b < a
 		int sum = 0;
 		for (int iter = abs(b); iter > 0; --iter)
 			sum += a;
 		if (b < 0)
-			sum = FnNegate(sum);
+			sum = negate(sum);
 		return sum;
 	}
 
 	// returns 1, if a/b >= 0.5, and 0 otherwise
-	public static int DefineAndRoundFraction(int a, int b) {
-		if (FnTimes(abs(a), 2) >= abs(b))
-			return 1;
-		else
-			return 0;
-	}
+	// public static int DefineAndRoundFraction(int a, int b) {
+	// if (multiply(abs(a), 2) >= abs(b))
+	// return 1;
+	// else
+	// return 0;
+	// }
 
 	/*
 	 * Divide a by b by literally counting how many times does b go into a. That
 	 * is, count how many times you can subtract b from a until you hit 0.
 	 */
-	public static int FnDivide(int a, int b)
-			throws java.lang.ArithmeticException {
+	public static int divide(int a, int b) throws java.lang.ArithmeticException {
 		if (b == 0) {
 			throw new java.lang.ArithmeticException("ERROR: Divide by zero.");
 		}
 		int quotient = 0;
-		int divisor = FnNegate(abs(b));
+		int divisor = negate(abs(b));
 		int divend; /* dividend */
 		for (divend = abs(a); divend >= abs(divisor); divend += divisor) {
 			++quotient;
 		}
-		if (DifferentSigns(a, b))
-			quotient = FnNegate(quotient);
+		if (diffSign(a, b))
+			quotient = negate(quotient);
 		return quotient;
 	}
 
@@ -80,34 +79,37 @@ public class Question {
 	}
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++) {
-			int a = randomInt(10);
+		for (int i = 0; i < 5; i++) {
+			int a = randomInt(50);
 			int b = randomInt(10);
-			int ans = FnMinus(a, b);
+			int ans = minus(a, b);
 			if (ans != a - b) {
 				System.out.println("ERROR");
 			}
-			System.out.println(a + " - " + b + " = " + ans);
+			System.out.print(a + " - " + b + " = " + ans + ";  ");
 		}
-		for (int i = 0; i < 100; i++) {
-			int a = randomInt(10);
+		System.out.println();
+		for (int i = 0; i < 5; i++) {
+			int a = randomInt(50);
 			int b = randomInt(10);
-			int ans = FnTimes(a, b);
+			int ans = multiply(a, b);
 			if (ans != a * b) {
 				System.out.println("ERROR");
 			}
-			System.out.println(a + " * " + b + " = " + ans);
+			System.out.print(a + " * " + b + " = " + ans + ";  ");
 		}
-		for (int i = 0; i < 100; i++) {
+		System.out.println();
+		for (int i = 0; i < 5; i++) {
 			int a = randomInt(10) + 1;
 			int b = randomInt(10) + 1;
 			System.out.print(a + " / " + b + " = ");
-			int ans = FnDivide(a, b);
+			int ans = divide(a, b);
 			if (ans != a / b) {
 				System.out.println("ERROR");
 			}
-			System.out.println(ans);
+			System.out.print(ans + ";  ");
 		}
+		System.out.println();
 	}
 
 }
