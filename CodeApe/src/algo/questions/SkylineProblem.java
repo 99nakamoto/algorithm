22ec58bd36c5af96dda9cade1db14bb52eb65100
abc <abc@abc.com>
2015-02-01 20:07:49 +0800

    d

---
+++ CodeApe/src/algo/questions/SkylineProblem.java
@@ -0,0 +1,13 @@
+package algo.questions;
+
+public class SkylineProblem {
+
+	public static void main(String[] args) {
+		SkylineProblem ins = new SkylineProblem();
+		System.out.println();
+	}
+
+	public void solve(int A) {
+
+	}
+}
