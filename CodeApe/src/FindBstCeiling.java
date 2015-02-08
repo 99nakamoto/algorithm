ae8cd8856c24db28051c580fdb9384073aec87f2
CodeApe <codeaperan@gmail.com>
2015-02-09 01:00:59 +0800

    d

---
+++ CodeApe/src/FindBstCeiling.java
@@ -0,0 +1,31 @@
+import common.Common;
+import common.TreeNode;
+
+public class FindBstCeiling {
+
+	public static void main(String[] args) {
+		FindBstCeiling ins = new FindBstCeiling();
+		long startTime = System.currentTimeMillis();
+
+		System.out.println("Code Ape Run... ");
+		TreeNode tree = Common.constructBstFromPreorder(new int[] { 40, 20, 10,
+				30, 60, 50, 70 });
+		for (int i = 0; i < 100; i++) {
+			System.out.println("The ceiling of " + i + " is  "
+					+ ins.findCeil(tree, i, Integer.MAX_VALUE));
+		}
+
+		System.out.print("Total time = ");
+		System.out.print((System.currentTimeMillis() - startTime) / 1000.0);
+	}
+
+	public int findCeil(TreeNode node, int num, int found) {
+		if (node != null) {
+			if (num > node.val)
+				return findCeil(node.right, num, found);
+			else
+				return findCeil(node.left, num, node.val);
+		} else
+			return found;
+	}
+}
