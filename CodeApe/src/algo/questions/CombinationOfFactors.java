873e88816c3cfa64737d2a80eace1c1aeab81287
Michael <theimmortalfish@gmail.com>
2015-02-15 09:19:45 +0800

    Signed-off-by: CodeApe <codeaperan@gmail.com>

---
+++ CodeApe/src/algo/questions/CombinationOfFactors.java
@@ -0,0 +1,46 @@
+package algo.questions;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationOfFactors {
+
+	public static void main(String[] args) {
+		CombinationOfFactors ins = new CombinationOfFactors();
+		long startTime = System.currentTimeMillis();
+
+		System.out.println("Code Ape Run... ");
+
+		List<List<Integer>> ans = ins.factorCombinations(24);
+		for (List<Integer> list : ans) {
+			for (int i : list)
+				System.out.print(i + " ");
+			System.out.println();
+		}
+		System.out.println();
+
+		System.out.print("Total time = ");
+		System.out.print((System.currentTimeMillis() - startTime) / 1000.0);
+	}
+
+	public List<List<Integer>> factorCombinations(int n) {
+		List<List<Integer>> ans = new ArrayList<List<Integer>>();
+		helper(ans, n, n / 2, new ArrayList<Integer>());
+		return ans;
+	}
+
+	private void helper(List<List<Integer>> ans, int num, int largestFactor,
+			List<Integer> path) {
+		if (num == 1) {
+			ans.add(new ArrayList<Integer>(path));
+			return;
+		}
+		for (int i = largestFactor; i > 1; i--) {
+			if (num % i == 0) {
+				path.add(i);
+				helper(ans, num / i, i, path);
+				path.remove(path.size() - 1);
+			}
+		}
+	}
+
+}
--- CodeApe/src/algo/questions/SumIntegerWeightedByDepth.java
+++ CodeApe/src/algo/questions/SumIntegerWeightedByDepth.java
@@ -1,6 +1,17 @@
 package algo.questions;
 
+import java.util.ArrayList;
 import java.util.List;
+
+/*
+ * LinkedIn question
+ * 
+ * Given a nested list of integers, returns the sum of all integers in the
+ * list weighted by their depth For example, given the list {{1,1},2,{1,1}}
+ * the function should return 10 (four 1's at depth 2, one 2 at depth 1)
+ * Given the list {1,{4,{6}}} the function should return 27 (one 1 at depth
+ * 1, one 4 at depth 2, and one 6 at depth 3)
+ */
 
 /*
  * This is the interface that represents nested lists. You should not implement
@@ -28,38 +39,41 @@ interface NestedInteger {
 	List<NestedInteger> getList();
 }
 
-public class SumIntegerWeightedByDepth implements NestedInteger {
+class NestedIntegerImpl implements NestedInteger {
 
-	/*
-	 * Given a nested list of integers, returns the sum of all integers in the
-	 * list weighted by their depth For example, given the list {{1,1},2,{1,1}}
-	 * the function should return 10 (four 1's at depth 2, one 2 at depth 1)
-	 * Given the list {1,{4,{6}}} the function should return 27 (one 1 at depth
-	 * 1, one 4 at depth 2, and one 6 at depth 3)
-	 */
-	public int depthSum(List<NestedInteger> input) {
-		// ur implementation here
+	int num;
+	List<NestedInteger> list = new ArrayList<NestedInteger>();
 
-		return 0;
+	public NestedIntegerImpl(int number) {
+		num = number;
+		list = null;
+	}
+
+	public NestedIntegerImpl(List<NestedInteger> inputList) {
+		num = -1;
+		list = inputList;
 	}
 
 	@Override
 	public boolean isInteger() {
-		// TODO Auto-generated method stub
-		return false;
+		return list == null;
 	}
 
 	@Override
 	public Integer getInteger() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isInteger()) {
+			return num;
+		}
+		return -1;
 	}
 
 	@Override
 	public List<NestedInteger> getList() {
-		// TODO Auto-generated method stub
-		return null;
+		return list;
 	}
+}
+
+public class SumIntegerWeightedByDepth {
 
 	public static void main(String[] args) {
 		SumIntegerWeightedByDepth ins = new SumIntegerWeightedByDepth();
@@ -67,7 +81,37 @@ public class SumIntegerWeightedByDepth implements NestedInteger {
 
 		System.out.println("Code Ape Run... ");
 
+		// build this: {1, {4, {6}}}
+		NestedInteger level1 = new NestedIntegerImpl(
+				new ArrayList<NestedInteger>());
+		NestedInteger level2 = new NestedIntegerImpl(
+				new ArrayList<NestedInteger>());
+		NestedInteger level3 = new NestedIntegerImpl(
+				new ArrayList<NestedInteger>());
+
+		level1.getList().add(new NestedIntegerImpl(1));
+		level1.getList().add(level2);
+		level2.getList().add(new NestedIntegerImpl(4));
+		level2.getList().add(level3);
+		level3.getList().add(new NestedIntegerImpl(6));
+
+		System.out.println("Sum = " + ins.depthSum(level1.getList(), 1));
+		System.out.println();
+
 		System.out.print("Total time = ");
 		System.out.print((System.currentTimeMillis() - startTime) / 1000.0);
+	}
+
+	public int depthSum(List<NestedInteger> input, int weight) {
+		// ur implementation here
+		int sum = 0;
+		for (NestedInteger ni : input) {
+			if (ni.isInteger()) {
+				sum += ni.getInteger() * weight;
+			} else {
+				sum += depthSum(ni.getList(), weight + 1);
+			}
+		}
+		return sum;
 	}
 }
