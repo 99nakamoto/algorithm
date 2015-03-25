f339fc0b9a07df5fb95cd3b1594f06052864949b
Michael <theimmortalfish@gmail.com>
2015-03-25 21:36:31 +0800

    d

--- CodeApe/src/Solution.java
+++ CodeApe/src/Solution.java
@@ -7,23 +7,31 @@ public class Solution {
 		int[] input = new int[] { 1, 0, 2, 0, 3, 0, 4 };
 		Common.printArray(input);
 
-		ins.method1(input);
-		Common.printArray(input);
+		System.out.println(ins.divide(Integer.MIN_VALUE, -1));
 	}
 
-	public void method1(int[] input) {
-		int p1 = 0;
-		int p2 = 0;
-		while (p2 < input.length) {
-			if (input[p2] == 0) {
-				p2++;
-			} else {
-				input[p1++] = input[p2++];
+	int divide(int dividend, int divisor) {
+		assert (divisor != 0);
+
+		boolean flag = (dividend < 0) ^ (divisor < 0);
+		long a = Math.abs((long) dividend);
+		long b = Math.abs((long) divisor);
+		long res = 0;
+		int c = 1;
+		while ((b << c) <= a) {
+			++c;
+		}
+		--c;
+		for (int i = c; i >= 0; --i) {
+			if ((b << i) <= a) {
+				res += ((long) 1 << i); // from high to low
+				a -= (b << i); // reminder
 			}
 		}
-		while (p1 < input.length) {
-			input[p1++] = 0;
-		}
+		if (flag == true)
+			res = -res;
+		if (res > Integer.MAX_VALUE)
+			return Integer.MAX_VALUE;
+		return (int) res;
 	}
-
 }
