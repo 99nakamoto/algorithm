a6502175c962bace4fa3f68c0f681c9280d0c7e8
Hello World <abc@d.com>
2015-04-12 22:51:31 +0800

    started using IntelliJ as IDE, updated gitignore

--- CodeApe/src/Solution.java
+++ CodeApe/src/Solution.java
@@ -1,37 +1,78 @@
 import common.Common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
-	public static void main(String[] args) {
-		Solution ins = new Solution();
-		int[] input = new int[] { 1, 0, 2, 0, 3, 0, 4 };
-		Common.printArray(input);
+    public static void main(String[] args) {
+        Solution ins = new Solution();
+        System.out.println(ins.fractionToDecimal(1, 6));
+    }
 
-		System.out.println(ins.divide(Integer.MIN_VALUE, -1));
-	}
+    public String fractionToDecimal(int numerator, int denominator) {
+        int integer = numerator / denominator;
+        int reminder = numerator % denominator;
+        if (reminder == 0) {
+            return String.valueOf(integer);
+        }
+        return integer + "." + fraction(reminder, denominator);
+    }
 
-	int divide(int dividend, int divisor) {
-		assert (divisor != 0);
+    String fraction(int num, int denum) {
+        // eg. num = 1, denum = 4, should return "25"
 
-		boolean flag = (dividend < 0) ^ (divisor < 0);
-		long a = Math.abs((long) dividend);
-		long b = Math.abs((long) divisor);
-		long res = 0;
-		int c = 1;
-		while ((b << c) <= a) {
-			++c;
-		}
-		--c;
-		for (int i = c; i >= 0; --i) {
-			if ((b << i) <= a) {
-				res += ((long) 1 << i); // from high to low
-				a -= (b << i); // reminder
-			}
-		}
-		if (flag == true)
-			res = -res;
-		if (res > Integer.MAX_VALUE)
-			return Integer.MAX_VALUE;
-		return (int) res;
-	}
+        List<Pair> list = new ArrayList<Pair>();
+        String result = "";
+        while (num != 0) {
+            num *= 10;
+            int digit = num / denum;
+
+            // eg. 1 / 333 = (003), so the pairs would be like this:
+            // {10, 0}, {100, 0}, {1000, 3}, {10, 0}...
+            Pair cur = new Pair(num, digit);
+            num %= denum;
+
+            // now add cur Pair to the list
+            if (list.indexOf(cur) == -1) {
+                list.add(cur);
+            } else {
+                // found a recurring dicimal in the previous output stream
+                int pos = list.indexOf(cur);
+                for (int i = 0; i < pos; i++) {
+                    result += list.get(i).digit;
+                }
+                result += "(";
+                for (int i = pos; i < list.size(); i++) {
+                    result += list.get(i).digit;
+                }
+                result += ")";
+                break;
+            }
+        }
+
+        // if there is recurring digit, the result should have already been generated
+        if (result.length() == 0) {
+            for (Pair p : list) {
+                result += p.digit;
+            }
+        }
+        return result;
+    }
+}
+
+class Pair {
+    int num;
+    int digit;
+
+    public Pair(int a, int b) {
+        num = a;
+        digit = b;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Pair p = (Pair) obj;
+        return this.num == p.num && this.digit == p.digit;
+    }
 }
