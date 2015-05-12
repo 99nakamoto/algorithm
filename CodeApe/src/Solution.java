32b47be32bab0c57d9e8671f2eb84f764d54340c
Hello World <abc@d.com>
2015-05-12 15:17:15 +0800

    more questions

--- CodeApe/src/Solution.java
+++ CodeApe/src/Solution.java
@@ -1,78 +1,43 @@
 import common.Common;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Solution {
 
     public static void main(String[] args) {
-        Solution ins = new Solution();
-        System.out.println(ins.fractionToDecimal(1, 6));
+        Solution s = new Solution();
+        System.out.println(s.largestNumber(new int[]{
+                12, 123, 45
+        }));
     }
 
-    public String fractionToDecimal(int numerator, int denominator) {
-        int integer = numerator / denominator;
-        int reminder = numerator % denominator;
-        if (reminder == 0) {
-            return String.valueOf(integer);
+    public String largestNumber(int[] num) {
+        if (num == null || num.length == 0) {
+            return "0";
         }
-        return integer + "." + fraction(reminder, denominator);
-    }
-
-    String fraction(int num, int denum) {
-        // eg. num = 1, denum = 4, should return "25"
-
-        List<Pair> list = new ArrayList<Pair>();
-        String result = "";
-        while (num != 0) {
-            num *= 10;
-            int digit = num / denum;
-
-            // eg. 1 / 333 = (003), so the pairs would be like this:
-            // {10, 0}, {100, 0}, {1000, 3}, {10, 0}...
-            Pair cur = new Pair(num, digit);
-            num %= denum;
-
-            // now add cur Pair to the list
-            if (list.indexOf(cur) == -1) {
-                list.add(cur);
-            } else {
-                // found a recurring dicimal in the previous output stream
-                int pos = list.indexOf(cur);
-                for (int i = 0; i < pos; i++) {
-                    result += list.get(i).digit;
-                }
-                result += "(";
-                for (int i = pos; i < list.size(); i++) {
-                    result += list.get(i).digit;
-                }
-                result += ")";
-                break;
-            }
+        String[] strs = new String[num.length];
+        for (int i = 0; i < num.length; i++) {
+            strs[i] = String.valueOf(num[i]);
         }
+        Arrays.sort(strs, new LargerComparator());
 
-        // if there is recurring digit, the result should have already been generated
-        if (result.length() == 0) {
-            for (Pair p : list) {
-                result += p.digit;
-            }
+        String res = "";
+        for (String str : strs) {
+            res = res + str;
         }
-        return result;
-    }
-}
-
-class Pair {
-    int num;
-    int digit;
-
-    public Pair(int a, int b) {
-        num = a;
-        digit = b;
+        return res;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        Pair p = (Pair) obj;
-        return this.num == p.num && this.digit == p.digit;
+
+    class LargerComparator implements Comparator<String> {
+
+        public int compare(String a, String b) {
+            String first = a + b;
+            String second = b + a;
+            return second.compareTo(first);
+        }
     }
 }
