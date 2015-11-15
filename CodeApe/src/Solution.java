11cf2f3fc979ffdf285b10fce750cdae8809723d
Michael Ran <theimmortalfish@gmail.com>
2015-11-14 18:15:59 -0600

    a

--- CodeApe/src/Solution.java
+++ CodeApe/src/Solution.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
 
 	public static void main(String[] args) {
@@ -5,18 +9,85 @@ public class Solution {
 		long startTime = System.currentTimeMillis();
 
 		System.out.println("Start Run... ");
-		String infix, postfix;
 
-		infix = "";
-		postfix = ins.infixToPostfix(infix);
-		System.out.println(infix);
-		System.out.println(postfix);
+		// Meeting[] m = new Meeting[4];
+		// m[0] = new Meeting(1, 6);
+		// m[1] = new Meeting(2, 4);
+		// m[2] = new Meeting(3, 5);
+		// m[3] = new Meeting(5, 7);
+		// ins.minNumConf(m);
+		//
+		// m = new Meeting[4];
+		// m[0] = new Meeting(1, 6);
+		// m[1] = new Meeting(2, 4);
+		// m[2] = new Meeting(3, 5);
+		// m[3] = new Meeting(4, 7);
+		// ins.minNumConf(m);
+
+		List<Meeting> m = new ArrayList<Meeting>();
+		m.add(new Meeting(1, 6));
+		m.add(new Meeting(2, 4));
+		m.add(new Meeting(3, 5));
+		m.add(new Meeting(4, 7));
+		ins.minNumConf(m);
+		
+		m = new ArrayList<Meeting>();
+		m.add(new Meeting(1, 6));
+		m.add(new Meeting(2, 4));
+		m.add(new Meeting(3, 5));
+		m.add(new Meeting(5, 7));
+		ins.minNumConf(m);
+		
+		m = new ArrayList<Meeting>();
+		m.add(new Meeting(1, 6));
+		m.add(new Meeting(2, 4));
+		m.add(new Meeting(3, 5));
+		m.add(new Meeting(5, 7));
+		m.add(new Meeting(9, 10));
+		m.add(new Meeting(8, 11));
+		ins.minNumConf(m);
 
 		System.out.print("Total time = ");
 		System.out.print((System.currentTimeMillis() - startTime) / 1000.0);
 	}
 
-	public String infixToPostfix(String infix) {
-		return infix + "d";
+	public int minNumConf(List<Meeting> list) {
+		if (list == null || list.size() == 0) {
+			return 0;
+		}
+
+		Collections.sort(list);
+
+		int count = 0;
+		int scheduled = 0;
+		while (list.size() != 0) {
+			// get end time of the first un-scheduled meeting
+			int endTime = list.get(0).end;
+			count++;
+			for (int i = list.size() - 1; i >= 0; i--) {
+				if (list.get(i).start <= endTime && endTime <= list.get(i).end) {
+					list.remove(i);
+				}
+			}
+		}
+
+		System.out.println(count);
+		System.out.println("===============");
+		return count;
+	}
+}
+
+class Meeting implements Comparable<Meeting> {
+	int start;
+	int end;
+
+	public Meeting(int a, int b) {
+		start = a;
+		end = b;
+	}
+
+	@Override
+	public int compareTo(Meeting o) {
+		return this.end - o.end;
 	}
 }
---
+++ CodeApe/src/algo/questions/NumBusStations.java
@@ -0,0 +1,78 @@
+package algo.questions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NumBusStations {
+
+	public static void main(String[] args) {
+		NumBusStations ins = new NumBusStations();
+		long startTime = System.currentTimeMillis();
+
+		System.out.println("Start Run... ");
+		List<Meeting> input = new ArrayList<Meeting>();
+
+		input.add(new Meeting(1, 3));
+		input.add(new Meeting(4, 7));
+		input.add(new Meeting(2, 6));
+		input.add(new Meeting(6, 10));
+		System.out.println(ins.solve(input));
+		System.out.println("==============================");
+
+		System.out.print("Total time = ");
+		System.out.print((System.currentTimeMillis() - startTime) / 1000.0);
+	}
+
+	public int solve(List<Meeting> input) {
+		List<Event> events = new ArrayList<Event>();
+		for (Meeting i : input) {
+			events.add(new Event(i.start, 1));
+			events.add(new Event(i.end, -1));
+		}
+		Collections.sort(events);
+
+		for (Event e : events) {
+			System.out.print("(" + e.time + ",  " + e.diff + ") ");
+		}
+
+		int currentNeeds = 0;
+		int maxNeeds = 0;
+		for (Event e : events) {
+			currentNeeds += e.diff;
+			maxNeeds = Math.max(maxNeeds, currentNeeds);
+		}
+		return maxNeeds;
+	}
+}
+
+class Event implements Comparable<Event> {
+	int time;
+	int diff;
+	// variable diff have value of 1 or -1
+	// when = 1, it means starting a meeting and 1 more room needed
+	// when = -1, it means ending a meeting and 1 less room needed
+
+	public Event(int a, int b) {
+		time = a;
+		diff = b;
+	}
+
+	@Override
+	public int compareTo(Event o) {
+		if (this.time == o.time) {
+			return this.diff - o.diff;
+		}
+		return this.time - o.time;
+	}
+}
+
+class Meeting {
+	int start;
+	int end;
+
+	public Meeting(int a, int b) {
+		start = a;
+		end = b;
+	}
+}
