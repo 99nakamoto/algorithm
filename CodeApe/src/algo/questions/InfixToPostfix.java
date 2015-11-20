54fbd663abbf2c77dfad3dc059a16303a1638b4c
Michael Ran <theimmortalfish@gmail.com>
2015-11-19 21:03:12 -0600

    changed algo for the question

--- CodeApe/src/algo/questions/InfixToPostfix.java
+++ CodeApe/src/algo/questions/InfixToPostfix.java
@@ -1,7 +1,5 @@
 package algo.questions;
 
-import java.util.Stack;
-
 public class InfixToPostfix {
 
 	public static void main(String[] args) {
@@ -19,6 +17,30 @@ public class InfixToPostfix {
 	}
 
 	public String solve(String infix) {
-		return null;
+		StringBuilder sb = new StringBuilder();
+		// get next operand or non-operand
+		// notice that operand is >=1 chars
+		int p = 0;
+		int len = infix.length();
+		while (p != len) {
+			if (isDigit(infix.charAt(p))) {
+				// if char at p is a digit
+				int q = p;
+				while (q != len && isDigit(infix.charAt(q))) {
+					q++;
+				}
+				// it is a number in the range [p, q-1]
+				sb.append(infix.substring(p, q));
+				p = q;
+			} else {
+				// if char at p is + - * / or ( )
+				
+			}
+		}
+		return sb.toString();
+	}
+
+	private boolean isDigit(char ch) {
+		return '0' <= ch && ch <= '9';
 	}
 }
